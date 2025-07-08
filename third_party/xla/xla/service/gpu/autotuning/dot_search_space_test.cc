262719242234f02bb9b7c341a325107583b272be
Ilya Tikhonovskiy <loislo@google.com>
2025-07-08 03:43:56 -0700

    [XLA:GPU] Do not drop low occupancy configs if the search space is small
    Low occupancy configs happen when the output size is small. If the number of configs is small it still makes sense to keep these configs. The win in the compile time will be negligible.
    
    PiperOrigin-RevId: 780495863

--- third_party/xla/xla/service/gpu/autotuning/dot_search_space_test.cc
+++ third_party/xla/xla/service/gpu/autotuning/dot_search_space_test.cc
@@ -298,10 +298,9 @@ TEST_F(DotSearchSpaceTest,
        FindsUniqueOccupancyMaximizingTilingForSmallProblem) {
   TF_ASSERT_OK_AND_ASSIGN(
       std::unique_ptr<VerifiedHloModule> module,
-      GetDefaultDotModule(/*lhs_parallel_dim=*/32, /*rhs_parallel_dim=*/32,
+      GetDefaultDotModule(/*lhs_parallel_dim=*/64, /*rhs_parallel_dim=*/64,
                           /*contracting_dim=*/32));
   TritonDotFusionSearchSpace search_space = MakeSearchSpace(module.get());
-
   EXPECT_THAT(search_space.GenerateConfigs(),
               AllOf(SizeIs(1), Each(AllOf(BlockMIs(Eq(16)), BlockNIs(Eq(16)),
                                           SplitKIs(Eq(2))))));
